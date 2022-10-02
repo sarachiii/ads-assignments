f24fd1187bb4e7aa9f5541ebaa0bd6ee2e60f780
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2022-10-02 13:50:02 +0200

    Made the toStrings and fromLine method

--- Assignment2/Assignment-2-TrafficControl_starter-v22.1/src/main/java/models/Detection.java
+++ Assignment2/Assignment-2-TrafficControl_starter-v22.1/src/main/java/models/Detection.java
@@ -1,8 +1,10 @@
 package models;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Locale;
 
 import static models.Car.CarType;
 import static models.Car.FuelType;
@@ -25,24 +27,49 @@ public class Detection {
     /**
      * Parses detection information from a line of text about a car that has entered an environmentally controlled zone
      * of a specified city.
-     * the format of the text line is: lisensePlate, city, dateTime
+     * the format of the text line is: licensePlate, city, dateTime
      * The licensePlate shall be matched with a car from the provided list.
-     * If no matching car can be found, a new Car shall be instantiated with the given lisensePlate and added to the list
+     * If no matching car can be found, a new Car shall be instantiated with the given licensePlate and added to the list
      * (besides the license plate number there will be no other information available about this car)
      * @param textLine
      * @param cars     a list of known cars, ordered and searchable by licensePlate
-     *                 (i.e. the indexOf method of the list shall only consider the lisensePlate when comparing cars)
+     *                 (i.e. the indexOf method of the list shall only consider the licensePlate when comparing cars)
      * @return a new Detection instance with the provided information
      * or null if the textLine is corrupt or incomplete
      */
     public static Detection fromLine(String textLine, List<Car> cars) {
+
         Detection newDetection = null;
 
-        // TODO convert the information in the textLine into a new Detection instance
-        //  use the cars.indexOf to find the car that is associated with the licensePlate of the detection
-        //  if no car can be found a new Car shall be instantiated and added to the list and associated with the detection
+        // extract the comma-separated fields from the textLine
+        String[] fields = textLine.split(",");
+        if(fields.length < 3) return null; // If the textLine is incomplete return null
+        else {
+            try {
+                String licensePlate = fields[0].trim(); // Get the licensePlate number from the textLine
 
+                Car newCar = new Car(licensePlate);
 
+                // If car is matched with the licensePlate it will be the new car,
+                // otherwise the newCar with only the licensePlate will be added to the cars list
+                if (cars.indexOf(newCar) != -1) {
+                    newCar = cars.get(cars.indexOf(newCar));
+                } else {
+                    cars.add(newCar);
+                }
+
+                // parse the fields and instantiate a new Detection
+                newDetection = new Detection(
+                        newCar,
+                        fields[1].trim(),
+                        LocalDateTime.parse(fields[2].trim())
+                );
+            } catch (Exception e) {
+                // any of the parse and valueOf methods could throw an exception on a format mismatch
+                System.out.printf("Could not parse Detection specification in text line '%s'\n", textLine);
+                System.out.println(e.getMessage());
+            }
+        }
         return newDetection;
     }
 
@@ -72,12 +99,9 @@ public class Detection {
         return dateTime;
     }
 
-
     @Override
     public String toString() {
-        // TODO represent the detection in the format: licensePlate/city/dateTime
-
-        return "TODO:Detection.toString";       // replace by a proper outcome
+        return car.getLicensePlate() + "/" + city + "/" + dateTime;
     }
 
 }
